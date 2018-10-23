82f00f54e8e87dd7bcbe1e64bf92511eee23d010
Amrit <amrit.pati@gmail.com>
2018-10-23 22:01:42 +0530

    Purge scripts consolidated
    combined the purge by URL and CP codes in one script.

---
+++ PURGE/purge.sh
@@ -0,0 +1,17 @@
+echo "Make sure to have the list of urls/cp codes in each of the respective 'purge_urls.json' or 'purge_cpcode.json' files before you proceed..."
+
+read -p "Purge by ? | Type cpcode or url: " purgeby
+read -p "Purge on network? | Type staging or production: " network
+read -p "Purge method? | Type invalidate or delete: " method
+
+echo "Issuing purge..."
+
+if [ "$purgeby" = url ]; then
+
+	http -a purge: POST :"/ccu/v3/$method/$purgeby/$network" "Content-Type:application/json" < purge_urls.json
+else 
+	http -a purge: POST :"/ccu/v3/$method/$purgeby/$network" "Content-Type:application/json" < purge_cpcode.json
+
+fi
+
+echo "Purge complete..."
