fbd9ad07035e6d2d4b15af9e736839e97c05fc84
Amrit <amrit.pati@gmail.com>
2018-10-23 22:07:19 +0530

    Removing unnecessary purge scripts
    Removing unnecessary purge scripts

--- PURGE/purge_deletebycpcode.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -ne 1 ]; then 
-	echo "Usage: 1 argument->network|staging or production "
-	exit 1
-fi	
-
-network=$1
-
-http -a purge: POST :"/ccu/v3/delete/cpcode/staging" "Content-Type:application/json" < purge_cpcode.json
--- PURGE/purge_invalidatebyurls.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -ne 1 ]; then 
-	echo "Usage: 1 argument->network|staging or production "
-	exit 1
-fi	
-
-network=$1
-
-http -a purge: POST :"/ccu/v3/invalidate/url/$network" "Content-Type:application/json" < purge_urls.json
